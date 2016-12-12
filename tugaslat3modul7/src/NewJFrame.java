097f681c15174e699bd0663840aea9e352fef974
Fariz <Schiffer_1945@yahoo.com>
2016-12-12 18:08:49 +0700

    Finish

--- tugaslat3modul7/src/NewJFrame.java
+++ tugaslat3modul7/src/NewJFrame.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JOptionPane;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -29,25 +32,28 @@ public class NewJFrame extends javax.swing.JFrame {
         jTabbedPane1 = new javax.swing.JTabbedPane();
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jTextField2 = new javax.swing.JTextField();
+        tbPBalok = new javax.swing.JTextField();
+        tbLBalok = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
-        jTextField3 = new javax.swing.JTextField();
+        tbTBalok = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
+        BtnBalok = new javax.swing.JButton();
         jPanel2 = new javax.swing.JPanel();
         jLabel7 = new javax.swing.JLabel();
-        jTextField4 = new javax.swing.JTextField();
+        tbPKubus = new javax.swing.JTextField();
         jLabel8 = new javax.swing.JLabel();
+        btnKubus = new javax.swing.JButton();
         jPanel3 = new javax.swing.JPanel();
         jLabel9 = new javax.swing.JLabel();
-        jTextField5 = new javax.swing.JTextField();
+        tbJBola = new javax.swing.JTextField();
         jLabel10 = new javax.swing.JLabel();
+        btnBola = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
         getContentPane().setLayout(null);
 
         jPanel1.setLayout(null);
@@ -55,10 +61,10 @@ public class NewJFrame extends javax.swing.JFrame {
         jLabel1.setText("Masukan panjang :");
         jPanel1.add(jLabel1);
         jLabel1.setBounds(10, 31, 140, 30);
-        jPanel1.add(jTextField1);
-        jTextField1.setBounds(150, 30, 220, 30);
-        jPanel1.add(jTextField2);
-        jTextField2.setBounds(150, 70, 220, 30);
+        jPanel1.add(tbPBalok);
+        tbPBalok.setBounds(150, 30, 220, 30);
+        jPanel1.add(tbLBalok);
+        tbLBalok.setBounds(150, 70, 220, 30);
 
         jLabel2.setText("Masukan lebar :");
         jPanel1.add(jLabel2);
@@ -67,8 +73,8 @@ public class NewJFrame extends javax.swing.JFrame {
         jLabel3.setText("Masukan tinggi :");
         jPanel1.add(jLabel3);
         jLabel3.setBounds(10, 110, 140, 30);
-        jPanel1.add(jTextField3);
-        jTextField3.setBounds(150, 110, 220, 30);
+        jPanel1.add(tbTBalok);
+        tbTBalok.setBounds(150, 110, 220, 30);
 
         jLabel4.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
         jLabel4.setText("cm");
@@ -85,10 +91,15 @@ public class NewJFrame extends javax.swing.JFrame {
         jPanel1.add(jLabel6);
         jLabel6.setBounds(380, 70, 60, 30);
 
-        jButton1.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
-        jButton1.setText("Hitung");
-        jPanel1.add(jButton1);
-        jButton1.setBounds(150, 160, 100, 40);
+        BtnBalok.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        BtnBalok.setText("Hitung");
+        BtnBalok.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BtnBalokActionPerformed(evt);
+            }
+        });
+        jPanel1.add(BtnBalok);
+        BtnBalok.setBounds(150, 160, 100, 40);
 
         jTabbedPane1.addTab("Balok", jPanel1);
 
@@ -97,13 +108,23 @@ public class NewJFrame extends javax.swing.JFrame {
         jLabel7.setText("Masukan panjang sisi :");
         jPanel2.add(jLabel7);
         jLabel7.setBounds(10, 31, 140, 30);
-        jPanel2.add(jTextField4);
-        jTextField4.setBounds(150, 30, 220, 30);
+        jPanel2.add(tbPKubus);
+        tbPKubus.setBounds(150, 30, 220, 30);
 
         jLabel8.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
         jLabel8.setText("cm");
         jPanel2.add(jLabel8);
         jLabel8.setBounds(380, 30, 60, 30);
+
+        btnKubus.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        btnKubus.setText("Hitung");
+        btnKubus.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnKubusActionPerformed(evt);
+            }
+        });
+        jPanel2.add(btnKubus);
+        btnKubus.setBounds(150, 80, 100, 40);
 
         jTabbedPane1.addTab("Kubus", jPanel2);
 
@@ -112,13 +133,23 @@ public class NewJFrame extends javax.swing.JFrame {
         jLabel9.setText("Masukan jari-jari :");
         jPanel3.add(jLabel9);
         jLabel9.setBounds(10, 31, 140, 30);
-        jPanel3.add(jTextField5);
-        jTextField5.setBounds(150, 30, 220, 30);
+        jPanel3.add(tbJBola);
+        tbJBola.setBounds(150, 30, 220, 30);
 
         jLabel10.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
         jLabel10.setText("cm");
         jPanel3.add(jLabel10);
         jLabel10.setBounds(380, 30, 60, 30);
+
+        btnBola.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        btnBola.setText("Hitung");
+        btnBola.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnBolaActionPerformed(evt);
+            }
+        });
+        jPanel3.add(btnBola);
+        btnBola.setBounds(150, 80, 100, 40);
 
         jTabbedPane1.addTab("Bola", jPanel3);
 
@@ -127,6 +158,51 @@ public class NewJFrame extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void BtnBalokActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtnBalokActionPerformed
+        // TODO add your handling code here:
+        
+        int panjang, lebar, tinggi;
+        panjang = Integer.parseInt(tbPBalok.getText());
+        lebar = Integer.parseInt(tbLBalok.getText());
+        tinggi = Integer.parseInt(tbTBalok.getText());
+        int hasil = panjang * lebar * tinggi;
+        
+        try {
+            JOptionPane.showMessageDialog(null, "Volume balok adalah : " + hasil, "luas",JOptionPane.INFORMATION_MESSAGE);
+        }
+        catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error : "+e,"Gagal", JOptionPane.WARNING_MESSAGE);
+        }
+    }//GEN-LAST:event_BtnBalokActionPerformed
+
+    private void btnKubusActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnKubusActionPerformed
+        // TODO add your handling code here:
+        int sisi;
+        sisi = Integer.parseInt(tbPKubus.getText());
+        int hasil = sisi * sisi * sisi;
+        
+        try {
+            JOptionPane.showMessageDialog(null, "Luas Permukaan kubus adalah :"+hasil,"luas", JOptionPane.INFORMATION_MESSAGE);
+        }
+        catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error :"+e,"Gagal",JOptionPane.WARNING_MESSAGE);
+        }
+    }//GEN-LAST:event_btnKubusActionPerformed
+
+    private void btnBolaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBolaActionPerformed
+        // TODO add your handling code here:
+        float jari;
+        jari = Integer.parseInt(tbJBola.getText());
+        float hasil = (float) (4.3*3.14*jari*jari*jari);
+        
+        try {
+            JOptionPane.showMessageDialog(null, "Luas permukaan bola adalah :"+hasil,"luas",JOptionPane.INFORMATION_MESSAGE);
+        }
+        catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error :"+e,"Gagal",JOptionPane.WARNING_MESSAGE);
+        }
+    }//GEN-LAST:event_btnBolaActionPerformed
 
     /**
      * @param args the command line arguments
@@ -164,7 +240,9 @@ public class NewJFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton BtnBalok;
+    private javax.swing.JButton btnBola;
+    private javax.swing.JButton btnKubus;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel2;
@@ -179,10 +257,10 @@ public class NewJFrame extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JTabbedPane jTabbedPane1;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
-    private javax.swing.JTextField jTextField4;
-    private javax.swing.JTextField jTextField5;
+    private javax.swing.JTextField tbJBola;
+    private javax.swing.JTextField tbLBalok;
+    private javax.swing.JTextField tbPBalok;
+    private javax.swing.JTextField tbPKubus;
+    private javax.swing.JTextField tbTBalok;
     // End of variables declaration//GEN-END:variables
 }
